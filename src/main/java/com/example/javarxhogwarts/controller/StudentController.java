52b7181bffc850641d5081e3922e1341d7f1b05a
nepster <gabrielnepster1@gmail.com>
2021-09-03 20:35:31 -0300

    arrumando observable

--- src/main/java/com/example/javarxhogwarts/controller/StudentController.java
+++ src/main/java/com/example/javarxhogwarts/controller/StudentController.java
@@ -1,20 +1,22 @@
 package com.example.javarxhogwarts.controller;
 
 import com.example.javarxhogwarts.Response.StudentResponse;
-import com.example.javarxhogwarts.repository.StudentRepository;
 import com.example.javarxhogwarts.request.StudentRequest;
-import com.example.javarxhogwarts.service.ConsumingApi;
 import com.example.javarxhogwarts.service.StudentService;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.StopWatch;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/aluno")
+@Slf4j
 public class StudentController {
 
     private final StudentService studentService;
@@ -28,7 +30,12 @@ public class StudentController {
     @GetMapping("/alunos")
     @ResponseStatus(HttpStatus.OK)
     public Observable<?> findAll() {
-        return studentService.findAll();
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Observable<?> all = studentService.findAll();
+        stopWatch.stop();
+        log.info("Executou em {}ms",stopWatch.getTime(TimeUnit.MILLISECONDS));
+        return all;
     }
 
     @GetMapping("/findByName/{name}")
--- src/main/java/com/example/javarxhogwarts/repository/StudentRepository.java
+++ src/main/java/com/example/javarxhogwarts/repository/StudentRepository.java
@@ -4,6 +4,8 @@ import com.example.javarxhogwarts.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.stream.Stream;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
 
